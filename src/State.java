ee9423b10cbb6173f47ae9934f3d7c01e0b8d6af
Pedro <Pedro@DESKTOP-U9K26GE>
2017-09-28 22:04:47 -0500

    Beginner decision and heuristic complete
    State Class and Beginner decision complete
    new class OpenRow, it contains information about an open row of the
    state.
    
    Use state.getOpenRows for an array of open rows and informations.
    
    other new functions in state:
    
    getHeuristic
    getBlanks
    get
    
    new functions in Main
    
    play(player1, player2)

--- src/State.java
+++ src/State.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 
 public class State {
 
@@ -5,18 +7,23 @@ public class State {
 	private static final int width = 6;
 	private static final int height = 5;
 	
-	private static final int NO_OPEN = 0;
-	private static final int ROW2 = 1;
-	private static final int ROW3 = 2;
-	
-	private static final int BLANK = 3;
-	private static final int SAME = 4;
-	private static final int DIFFERENT = 5;
+	private static final int BLANK = 0;
+	private static final int SAME = 1;
+	private static final int DIFFERENT = 2;
 	
 	public State() {
 		board = new char[height][width];
+		reset();
 	}
 	
+	public static int getWidth() {
+		return width;
+	}
+
+	public static int getHeight() {
+		return height;
+	}
+
 	public State(char[][] board) {
 		this.board = board;
 	}
@@ -41,57 +48,58 @@ public class State {
 		}
 	}
 	
+	public ArrayList<int[]> getBlanks() {
+		ArrayList<int[]> blanks = new ArrayList<>();
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (board[i][j] == ' ') {
+					blanks.add(new int[] {i,j});
+				}
+			}
+		}
+		return blanks;
+	}
+	
+	public void newMove(char playerChar, int i, int j) {
+		board[i][j] = playerChar;
+	}
+	
+	public char get(int i, int j) {
+		return board[i][j];
+	}
+	
 	public int getHeuristic(char player) {
 		int threeForX = 0;
 		int twoForX = 0;
 		int threeForO = 0;
 		int twoForO = 0;
-		for (int i = 0; i < height; i++) {
-			for (int j = 0; j < width; j++) {
-				if (j+2 <= width) {
-					int horizontalRow = getOpenRow("horizontal", i, j, 'X');
-					if (horizontalRow == ROW3) threeForX += 1;
-					else if (horizontalRow == ROW2) twoForX += 1;
-					
-					horizontalRow = getOpenRow("horizontal", i, j, 'O');
-					if (horizontalRow == ROW3) threeForO += 1;
-					else if (horizontalRow == ROW2) twoForO += 1;
+		ArrayList<OpenRow> openRows = getOpenrows();
+		for (OpenRow or: openRows) {
+			if (or.getPlayer() == 'X') {
+				if (or.getType() == 2) {
+					twoForX++;
 				}
-				if (j+2 <= width && i+2 <= height) {
-					int diagonalRow = getOpenRow("diagonalright", i, j, 'X');
-					if (diagonalRow == ROW3) threeForX += 1;
-					else if (diagonalRow == ROW2) twoForX += 1;
-					
-					diagonalRow = getOpenRow("diagonalright", i, j, 'O');
-					if (diagonalRow == ROW3) threeForO += 1;
-					else if (diagonalRow == ROW2) twoForO += 1;
+				else if (or.getType() == 3) {
+					threeForX++;
 				}
-				if (j-2 > 0 && i+2 <= height) {
-					int diagonalRow = getOpenRow("diagonalleft", i, j, 'X');
-					if (diagonalRow == ROW3) threeForX += 1;
-					else if (diagonalRow == ROW2) twoForX += 1;
-					
-					diagonalRow = getOpenRow("diagonalleft", i, j, 'O');
-					if (diagonalRow == ROW3) threeForO += 1;
-					else if (diagonalRow == ROW2) twoForO += 1;
+			}
+			else if (or.getPlayer() == 'O') {
+				if (or.getType() == 2) {
+					twoForO++;
 				}
-				if (i+2 <= height) {
-					int verticalRow = getOpenRow("vertical", i, j, 'X');
-					if (verticalRow == ROW3) threeForX += 1;
-					else if (verticalRow == ROW2) twoForX += 1;
-					
-					verticalRow = getOpenRow("vertical", i, j, 'O');
-					if (verticalRow == ROW3) threeForO += 1;
-					else if (verticalRow == ROW2) twoForO += 1;
+				else if (or.getType() == 3) {
+					threeForO++;
 				}
 			}
 		}
+		
 		/*
 		System.out.println("threeForX " + threeForX);
 		System.out.println("twoForX " + twoForX);
 		System.out.println("threeForO " + threeForO);
 		System.out.println("twoForO " + twoForO);
 		*/
+		
 		int k = 1;
 		if (player == 'O') {
 			k = -1;
@@ -99,15 +107,65 @@ public class State {
 		return (k*3*threeForX) - (k*3*threeForO) + (k*twoForX) - (k*twoForO);
 	}
 	
-	private int getOpenRow(String direction, int i, int j, char player) {
+	public ArrayList<OpenRow> getOpenrows() {
+		ArrayList<OpenRow> openRows = new ArrayList<>();
+		OpenRow newOpenRow;
+		for (int i = 0; i < height; i++) {
+			for (int j = 0; j < width; j++) {
+				if (j+2 <= width) {
+					newOpenRow = getOpenRow("horizontal", i, j, 'X');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+					newOpenRow = getOpenRow("horizontal", i, j, 'O');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+				}
+				if (j+2 <= width && i+2 <= height) {
+					newOpenRow = getOpenRow("diagonalright", i, j, 'X');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+					newOpenRow = getOpenRow("diagonalright", i, j, 'O');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+				}
+				if (j-2 > 0 && i+2 <= height) {
+					newOpenRow = getOpenRow("diagonalleft", i, j, 'X');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+					newOpenRow = getOpenRow("diagonalleft", i, j, 'O');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+				}
+				if (i+2 <= height) {
+					newOpenRow = getOpenRow("vertical", i, j, 'X');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+					newOpenRow = getOpenRow("vertical", i, j, 'O');
+					if (newOpenRow != null) {
+						openRows.add(newOpenRow);
+					}
+				}
+			}
+		}
+		return openRows;
+	}
+
+	private OpenRow getOpenRow(String direction, int i, int j, char player) {
 		int pos0 = getNext(0, direction, i, j, player);
 		int pos1 = getNext(1, direction, i, j, player);
 
 		if (pos0 != SAME) {
-			return NO_OPEN;
+			return null;
 		}
 		if (pos1 != SAME) {
-			return NO_OPEN;
+			return null;
 		}
 		
 		int posm1 = getNext(-1, direction, i, j, player);
@@ -116,49 +174,72 @@ public class State {
 
 		if (posm1 == BLANK) {
 			if (pos2 == BLANK) {
-				return ROW2;
+				Random rand = new Random();
+				return new OpenRow(player, 2, getCoordinate((rand.nextInt(1) == 0 ? -1 : 2), direction, i, j));
 			}
 			if (pos2 == SAME) {
 				if (pos3 == BLANK) {
-					return ROW3;
+					Random rand = new Random();
+					return new OpenRow(player, 3, getCoordinate((rand.nextInt(1) == 0 ? -1 : 3), direction, i, j));
 				}
-				if (pos3 == SAME) {
-					return NO_OPEN;
+				if (pos3 == SAME) {//there is a winner
+					return new OpenRow(player, 4, null);
 				}
 				if (pos3 == DIFFERENT) {
-					return ROW3;
+					return new OpenRow(player, 3, getCoordinate(-1, direction, i, j));
 				}
 			}
 			if (pos2 == DIFFERENT) {
-				return ROW2;
+				return new OpenRow(player, 2, getCoordinate(-1, direction, i, j));
 			}
 		}
 		if (posm1 == SAME) {
-			return NO_OPEN;
+			return null;
 		}
 		if (posm1 == DIFFERENT) {
 			if (pos2 == BLANK) {
-				return ROW2;
+				return new OpenRow(player, 2, getCoordinate(2, direction, i, j));
 			}
 			if (pos2 == SAME) {
 				if (pos3 == BLANK) {
-					return ROW3;
+					return new OpenRow(player, 3, getCoordinate(3, direction, i, j));
 				}
-				if (pos3 == SAME) {
-					return NO_OPEN;
+				if (pos3 == SAME) {//there is a winner
+					return new OpenRow(player, 4, null);
 				}
 				if (pos3 == DIFFERENT) {
-					return NO_OPEN;
+					return null;
 				}
 			}
 			if (pos2 == DIFFERENT) {
-				return NO_OPEN;
+				return null;
 			}
 		}
-		return NO_OPEN;
+		return null;
 	}
 	
 	private int getNext(int offset, String direction, int i, int j, char player) {
+		int [] newCoordinates = getCoordinate(offset, direction, i, j);
+		int newi = newCoordinates[0];
+		int newj = newCoordinates[1];
+		
+		if (newi < 0 || newi >= height) {
+			return DIFFERENT;
+		}
+		if (newj < 0 || newj >= width) {
+			return DIFFERENT;
+		}
+		else {
+			if (board[newi][newj] == ' ') {
+				return BLANK;
+			}
+			else {
+				return (board[newi][newj] == player) ? SAME : DIFFERENT;
+			}
+		}
+	}
+	
+	private int[] getCoordinate(int offset, String direction, int i, int j) {
 		int newi=0;
 		int newj=0;
 		if (direction.equals("horizontal")) {
@@ -177,22 +258,17 @@ public class State {
 			newi = i+offset;
 			newj = j;
 		}
-		
-		if (newi < 0 || newi >= height) {
-			return DIFFERENT;
-		}
-		if (newj < 0 || newj >= width) {
-			return DIFFERENT;
-		}
-		else {
-			if (board[newi][newj] == ' ') {
-				return BLANK;
-			}
-			else {
-				return (board[newi][newj] == player) ? SAME : DIFFERENT;
-			}
-		}
+		return new int[] {newi, newj};
 	}
-	
+
+	public char getWinner() {
+		ArrayList<OpenRow> openRows = getOpenrows();
+		for (OpenRow or: openRows) {
+			if (or.getType() == 4) {
+				return or.getPlayer();
+			}
+		}
+		return ' ';
+	}
 	
 }
