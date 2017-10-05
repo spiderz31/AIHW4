d2778fdaca1e43eb464fcf5a4901b19d3b4722d3
Pedro <Pedro@10.7.69.7>
2017-10-05 12:09:34 -0500

    more comments

--- src/OpenRow.java
+++ src/OpenRow.java
@@ -5,6 +5,13 @@
  * Joshua Lewis
  */
 
+/*
+ * For the calculation of open rows (open 2 in a row and open 3 in a row) we have implemented a class called “OpenRow”. 
+ * An OpenRow object has all the information of an open row: 
+ * 	The type of the open row: 2 (open 2 in a row) or 3 (open 3 in a row).
+ * 	The position of the blank  at one end of the open row. If it has 2 blank ends, it will return one of the two blanks positions at random.
+ * 	The player of whom belongs the open row. Could be ‘X’ or ‘O’. 
+ */
 public class OpenRow {
 	private char player;
 	private int type; //2 or 3. 4 if there is a winner
